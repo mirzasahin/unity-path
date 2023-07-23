c11a1ff1d4646580122c6115b73376be449c676e
Mirza Sahin <salihmirza123@gmail.com>
2023-07-23 16:31:20 +0300

    Add spawn manager

--- unity-learn/Assets/Scripts/DestroyOutOfBounds.cs
+++ unity-learn/Assets/Scripts/DestroyOutOfBounds.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
+    private float topBound = 25;
+    private float lowerBound = -10;
 
     // Start is called before the first frame update
     void Start()
@@ -14,7 +16,11 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.z > 25)
+        if(transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if(transform.position.z < lowerBound)
         {
             Destroy(gameObject);
         }
